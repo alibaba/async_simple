5caf5e8654a9b98531aabbc196aed2b646b94035
Chuanqi Xu <yedeng.yd@linux.alibaba.com>
2022-03-07 19:37:24 +0800

    Testing

--- benchmarks/Lazy.bench.cpp
+++ benchmarks/Lazy.bench.cpp
@@ -31,6 +31,10 @@ void async_simple_Lazy_collectAll(benchmark::State& state) {
     };
     for ([[maybe_unused]] const auto& _ : state)
         syncAwait(collectAllStarter());
+    for ([[maybe_unused]] const auto& _ : state)
+        syncAwait(collectAllStarter());
+    for ([[maybe_unused]] const auto& _ : state)
+        syncAwait(collectAllStarter());
 }
 
 BENCHMARK(async_simple_Lazy_chain);
