d505a2f0d747e1342792b47b3ae2b4f4b557a81a
qicosmos <383121719@qq.com>
2022-03-30 10:28:25 +0800

    fix test case (#81)
    * fix test case
    
    * use conditon wait

--- async_simple/coro/test/LazyTest.cpp
+++ async_simple/coro/test/LazyTest.cpp
@@ -27,6 +27,7 @@
 #include <async_simple/executors/SimpleExecutor.h>
 #include <async_simple/experimental/coroutine.h>
 #include <async_simple/test/unittest.h>
+#include <async_simple/util/Condition.h>
 #include <chrono>
 
 using namespace std;
@@ -1350,16 +1351,16 @@ TEST_F(LazyTest, testBatchedcollectAll) {
 }
 
 TEST_F(LazyTest, testDetach) {
+    util::Condition cond;
     int count = 0;
-    {
-        executors::SimpleExecutor e1(1);
-        auto test1 = [&count]() -> Lazy<int> {
-            count += 2;
-            co_return count;
-        };
-        test1().via(&e1).detach();
-    }
-
+    executors::SimpleExecutor e1(1);
+    auto test1 = [&]() -> Lazy<int> {
+        count += 2;
+        cond.release();
+        co_return count;
+    };
+    test1().via(&e1).detach();
+    cond.acquire();
     EXPECT_EQ(count, 2);
 }
 
