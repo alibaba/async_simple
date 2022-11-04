4d0fb0cb23e3f53a96f3853b80e57dcbe1d04182
4kangjc <1158035520@qq.com>
2022-11-04 11:15:20 +0800

    add macos x86_64 and aarch64 uthread support

--- async_simple/uthread/internal/thread_impl.h
+++ async_simple/uthread/internal/thread_impl.h
@@ -23,9 +23,6 @@
 #ifndef ASYNC_SIMPLE_UTHREAD_INTERNAL_UTHREAD_IMPL_H
 #define ASYNC_SIMPLE_UTHREAD_INTERNAL_UTHREAD_IMPL_H
 
-#include <setjmp.h>
-#include <ucontext.h>
-
 namespace async_simple {
 namespace uthread {
 namespace internal {
