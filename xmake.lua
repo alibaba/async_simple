c9a9ed7f5bb83e8f798cba62887040af268c79ad
ruki <waruqi@gmail.com>
2023-06-06 23:56:52 +0800

    improve xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -2,39 +2,43 @@ add_rules("mode.debug", "mode.release")
 
 set_languages("c++20")
 
-add_cxxflags("-stdlib=libstdc++")
 add_requires("gtest")
+if is_plat("linux") then
+    add_requires("libaio")
+    add_syslinks("pthread")
+    add_cxxflags("-stdlib=libstdc++")
+else
+    add_defines("ASYNC_SIMPLE_HAS_NOT_AIO")
+end
 
 target("async_simple")
     set_kind("static")
-    add_files("modules/async_simple.cppm",
-              "async_simple/executors/SimpleExecutor.cpp")
+    add_files("modules/async_simple.cppm")
+    add_files("async_simple/executors/SimpleExecutor.cpp")
     add_includedirs(".")
-    set_languages("c++20")
+    add_packages("libaio", {public = true})
 
 target("async_simple_coro_test")
+    add_deps("async_simple")
     add_files("async_simple/coro/test/*.cpp", "async_simple/test/dotest.cpp")
     add_defines("USE_MODULES")
     add_includedirs(".")
-    add_deps("async_simple")
-    add_links("gtest", "gmock", "async_simple", "aio", "pthread")
+    add_packages("gtest")
 
 target("async_simple_test")
+    add_deps("async_simple")
     add_files("async_simple/test/*.cpp")
     add_defines("USE_MODULES")
     add_includedirs(".")
-    add_deps("async_simple")
-    add_links("gtest", "gmock", "async_simple", "aio", "pthread")
+    add_packages("gtest")
 
 target("CountChar")
+    add_deps("async_simple")
     add_files("demo_example/CountChar.cpp")
     add_defines("USE_MODULES")
-    add_deps("async_simple")
-    add_links("async_simple")
 
 target("ReadFiles")
+    add_deps("async_simple")
     add_files("demo_example/ReadFiles.cpp")
     add_defines("USE_MODULES")
-    add_deps("async_simple")
-    add_links("async_simple", "aio", "pthread")
 
