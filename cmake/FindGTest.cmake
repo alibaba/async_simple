7084fc612741ce341c622176f652381453b0fe68
Connector Switch <c8ef@outlook.com>
2026-03-13 09:44:03 +0800

    ci: fix macos cmake build (#455)

--- cmake/FindGTest.cmake
+++ cmake/FindGTest.cmake
@@ -10,7 +10,7 @@ else()
     FetchContent_Declare(
         googletest
         GIT_REPOSITORY https://github.com/google/googletest.git
-        GIT_TAG release-1.11.0
+        GIT_TAG v1.17.0
         GIT_SHALLOW ON
     )
     if(CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND
